7e91a5ec6f4db6574c92b1700ce5e9140bdcba2d
Ian Partridge <i.partridge@uk.ibm.com>
2020-04-01 15:57:33 +0100

    second go

--- incubator/java-openliberty/templates/reactive/src/main/java/dev/appsody/starter/StarterApplication.java
+++ incubator/java-openliberty/templates/reactive/src/main/java/dev/appsody/starter/StarterApplication.java
@@ -1,9 +1,27 @@
 package dev.appsody.starter;
 
+import java.util.logging.Logger;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
 @ApplicationPath("/starter")
 public class StarterApplication extends Application {
+    private static Logger logger = Logger.getLogger(StarterApplication.class.getName());
 
+    @Incoming("incomingTopic1")
+    @Outgoing("outgoingTopic1")
+    public String processMessage(String message) {
+        logger.info("Message " + message);
+
+        return message;
+    }
+
+    @Incoming("incomingTopic2")
+    public void receiveMessage(String message) {
+        logger.info("Message " + message);
+    }
 }
